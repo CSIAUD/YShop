ccf4afdc820952768a68804b16d3c7e68685411d
Miisterone <miisterone27@gmail.com>
2023-11-28 14:23:23 +0100

    Fix sliderover

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,14 +4,13 @@ export const Navbar = ({ onCartClick }) => {
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-lg font-bold">YShop</h1>
                 <div className="flex items-center">
-                    <button className="flex items-center">
+                    <button className="flex items-center" onClick={onCartClick}>
                         <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7h18l-2 13H5L3 7z" />
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 00-8 0" />
                         </svg>
                         Panier
                     </button>
-                    <button onClick={onCartClick}>Panier</button>
                 </div>
             </div>
         </nav>
