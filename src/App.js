dda6080087dada0a924bd457ac710ea3808b5bf4
Joris P <71002181+Miisterone@users.noreply.github.com>
2023-11-30 14:51:27 +0100

    Fix notif

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import SlideOver from "./components/sliderover";
 const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    const [confirmDialog, setConfirmDialog] = useState(false);
-    const [productToAdd, setProductToAdd] = useState(null);
 
     function getProducts() {
         axios
@@ -30,14 +28,7 @@ const App = () => {
     }, []);
 
     const addToCart = (product) => {
-        setProductToAdd(product);
-        setConfirmDialog(true);
-    };
-
-    const confirmAddToCart = () => {
-        setCart([...cart, productToAdd]);
-        setProductToAdd(null);
-        setConfirmDialog(false);
+        setCart([...cart, product]);
     };
 
     const [isSlideOverOpen, setIsSlideOverOpen] = useState(false);
@@ -50,19 +41,6 @@ const App = () => {
         <div>
             <Navbar cart={cart} onCartClick={toggleSlideOver} />
             {isSlideOverOpen && <SlideOver cart={cart} setCart={setCart} />}
-            {confirmDialog && (
-                <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 z-50 shadow-lg rounded-lg">
-                    <p className="text-gray-700 text-lg font-medium">Êtes-vous sûr de vouloir ajouter ce produit au panier ?</p>
-                    <div className="flex justify-end space-x-2 mt-4">
-                        <button onClick={confirmAddToCart} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
-                            Oui
-                        </button>
-                        <button onClick={() => setConfirmDialog(false)} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300">
-                            Non
-                        </button>
-                    </div>
-                </div>
-            )}
 
             <div className="container mx-auto p-4">
                 <div className="grid grid-cols-3 gap-4">
--- src/components/sliderover.js
+++ src/components/sliderover.js
@@ -1,15 +1,20 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
+import {Fragment, useState} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
+import {XMarkIcon} from '@heroicons/react/24/outline'
 import axios from "axios";
 
 export default function SlideOver({cart = [], setCart}) {
     const [open, setOpen] = useState(true)
-    function removeFromCart(id) {
-       const newCart = cart.filter((product) => product.id !== id);
-         setCart(newCart);
+    const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+    const [selectedProductID, setSelectedProductID] = useState(null);
+    const [showNotification, setShowNotification] = useState(false);
 
+
+    function removeFromCart(id) {
+        const newCart = cart.filter((product) => product.id !== id);
+        setCart(newCart);
     }
+
     function postOrder() {
         const order = {
             id: cart.map((product) => product.id),
@@ -26,6 +31,15 @@ export default function SlideOver({cart = [], setCart}) {
                     console.error('La requête a échoué avec le code de statut:', response.status);
                 }
             })
+            .then((response) => {
+                if (response.status === 201) {
+                    setCart([]);
+                    setShowNotification(true);
+                    setTimeout(() => setShowNotification(false), 3000); // La notification disparaît après 3 secondes
+                } else {
+                    console.error('La requête a échoué avec le code de statut:', response.status);
+                }
+            })
             .catch((error) => {
                 console.error('Une erreur s\'est produite lors de la requête:', error);
             });
@@ -38,27 +52,43 @@ export default function SlideOver({cart = [], setCart}) {
         }, 0);
     }
 
+    function requestRemoveFromCart(id) {
+        setSelectedProductID(id);
+        setShowConfirmDelete(true);
+    }
+
+    function confirmRemoveFromCart() {
+        if (selectedProductID !== null) {
+            const newCart = cart.filter((product) => product.id !== selectedProductID);
+            setCart(newCart);
+            setShowConfirmDelete(false);
+        }
+    }
+
+
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={setOpen}>
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                <div className="fixed inset-0 overflow-hidden">
-                    <div className="absolute inset-0 overflow-hidden">
-                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <div>
+                <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+                    <div className="fixed inset-0 overflow-hidden">
+                        <div className="absolute inset-0 overflow-hidden">
+                            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
                                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
                                     <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                                         <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                                             <div className="flex items-start justify-between">
-                                                <Dialog.Title className="text-lg font-medium text-gray-900">Panier</Dialog.Title>
+                                                <Dialog.Title
+                                                    className="text-lg font-medium text-gray-900">Panier</Dialog.Title>
                                                 <div className="ml-3 flex h-7 items-center">
                                                     <button
                                                         type="button"
                                                         className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
                                                         onClick={() => setOpen(false)}
                                                     >
-                                                        <span className="absolute -inset-0.5" />
+                                                        <span className="absolute -inset-0.5"/>
                                                         <span className="sr-only">Close panel</span>
-                                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                                        <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
                                                     </button>
                                                 </div>
                                             </div>
@@ -69,9 +99,10 @@ export default function SlideOver({cart = [], setCart}) {
 
                                                         {cart.map((cart) => (
                                                             <li key={cart.id} className="flex py-6">
-                                                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                                                <div
+                                                                    className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                                                     <img
-                                                                        src={cart.imageSrc}
+                                                                        src={cart.imageUrl}
                                                                         alt={cart.imageAlt}
                                                                         className="h-full w-full object-cover object-center"
                                                                     />
@@ -79,7 +110,8 @@ export default function SlideOver({cart = [], setCart}) {
 
                                                                 <div className="ml-4 flex flex-1 flex-col">
                                                                     <div>
-                                                                        <div className="flex justify-between text-base font-medium text-gray-900">
+                                                                        <div
+                                                                            className="flex justify-between text-base font-medium text-gray-900">
                                                                             <h3>
                                                                                 <a href={cart.href}>{cart.name}</a>
                                                                             </h3>
@@ -87,14 +119,15 @@ export default function SlideOver({cart = [], setCart}) {
                                                                         </div>
                                                                         <p className="mt-1 text-sm text-gray-500">{cart.color}</p>
                                                                     </div>
-                                                                    <div className="flex flex-1 items-end justify-between text-sm">
-                                                                        <p className="text-gray-500">Qty {cart.quantity}</p>
+                                                                    <div
+                                                                        className="flex flex-1 items-end justify-between text-sm">
+                                                                        <p className="text-gray-500"></p>
 
                                                                         <div className="flex">
                                                                             <button
                                                                                 type="button"
                                                                                 className="font-medium text-indigo-600 hover:text-indigo-500"
-                                                                                onClick={() => removeFromCart(cart.id)}
+                                                                                onClick={() => requestRemoveFromCart(cart.id)}
                                                                             >
                                                                                 Remove
                                                                             </button>
@@ -114,19 +147,41 @@ export default function SlideOver({cart = [], setCart}) {
                                                 <p>{totalPrice.toFixed(2)} €</p>
                                             </div>
                                             <div className="mt-6">
-                                                <button disabled ={cart.length < 1} className={`flex items-center justify-center rounded-md border border-transparent px-6 py-3 text-base font-medium text-white shadow-sm ${
-                                                    cart.length < 1 ? 'bg-gray-500 hover:bg-gray-500' : 'bg-indigo-600 hover:bg-indigo-700'
-                                                }`} onClick={() => postOrder(cart.id)}>
+                                                <button disabled={cart.length < 1}
+                                                        className={`flex items-center justify-center rounded-md border border-transparent px-6 py-3 text-base font-medium text-white shadow-sm ${
+                                                            cart.length < 1 ? 'bg-gray-500 hover:bg-gray-500' : 'bg-indigo-600 hover:bg-indigo-700'
+                                                        }`} onClick={() => postOrder(cart.id)}>
                                                     Acheter
                                                 </button>
                                             </div>
                                         </div>
                                     </div>
                                 </Dialog.Panel>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Dialog>
+                </Dialog>
+                {showConfirmDelete && (
+                    <Dialog as="div" className="relative z-20" onClose={() => setShowConfirmDelete(false)}>
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75"/>
+                        <div className="fixed inset-0 flex items-center justify-center p-4">
+                            <Dialog.Panel className="w-full max-w-sm rounded bg-white p-6">
+                                <Dialog.Description>
+                                    Voulez-vous vraiment supprimer cet article ?
+                                </Dialog.Description>
+                                <div className="mt-4">
+                                    <button className="mr-2 bg-red-500 text-white px-4 py-2 rounded"
+                                            onClick={confirmRemoveFromCart}>Supprimer
+                                    </button>
+                                    <button className="bg-gray-300 px-4 py-2 rounded"
+                                            onClick={() => setShowConfirmDelete(false)}>Annuler
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </div>
+                    </Dialog>
+                )}
+            </div>
         </Transition.Root>
     )
 }
